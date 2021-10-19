e2f977ce3246a4b751486e1d9831455922859028
Sahan Fernando <sahan.h.fernando@gmail.com>
2021-10-19 17:08:34 +1100

    Move message thread indentation logic to slice

--- packages/frontend/src/features/timeline/Timeline.tsx
+++ packages/frontend/src/features/timeline/Timeline.tsx
@@ -1,69 +1,13 @@
-import { IMessage } from "models";
 import { FC, useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { timelineFetchAsync } from "./timelineSlice";
 import "./Timeline.scss";
 
-interface Thread {
-    first: number;
-    last: number;
-    count: number;
-}
-
-type ThreadLookup = { [threadId: string]: Thread };
-type ThreadIndentationLookup = { [threadId: string]: number };
-
-function determineHighlightedThreads(messages: IMessage[]): ThreadLookup {
-    const threads: { [threadId: string]: Thread } = {};
-    for (const message of messages) {
-        if (!threads[message.threadId]) {
-            threads[message.threadId] = {
-                first: message.timeRank,
-                last: message.timeRank,
-                count: 1,
-            };
-        } else {
-            threads[message.threadId].count += 1;
-            threads[message.threadId].last = message.timeRank;
-        }
-    }
-    for (const threadId in threads) {
-        if (threads[threadId].count < 2) {
-            delete threads[threadId];
-        }
-    }
-    return threads;
-}
-
-function determineIndentation(messages: IMessage[]): ThreadIndentationLookup {
-    const highlightedThreads = determineHighlightedThreads(messages);
-    const indentations: ThreadIndentationLookup = {};
-    const currentlyAllocated = new Set<number>();
-
-    for (const { threadId, timeRank } of messages) {
-        const messageThread = highlightedThreads[threadId];
-        if (messageThread) {
-            if (messageThread.first === timeRank) {
-                // Is first element of thread
-                for (let i = 1; i <= messages.length; ++i) {
-                    if (!currentlyAllocated.has(i)) {
-                        indentations[threadId] = i;
-                        currentlyAllocated.add(i);
-                        break;
-                    }
-                }
-            } else if (messageThread.last === timeRank) {
-                // Is last element of thread
-                currentlyAllocated.delete(indentations[threadId]);
-            }
-        }
-    }
-    return indentations;
-}
-
 export const Timeline: FC = () => {
     const messages = useAppSelector((state) => state.timeline.messages);
-    const threadIndentation = determineIndentation(Object.values(messages));
+    const threadIndentation = useAppSelector(
+        (state) => state.timeline.threadIndentation
+    );
     const dispatch = useAppDispatch();
     useEffect(() => {
         dispatch(timelineFetchAsync());
--- packages/frontend/src/features/timeline/timelineSlice.ts
+++ packages/frontend/src/features/timeline/timelineSlice.ts
@@ -2,14 +2,71 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { IMessage } from "models";
 import { fetchMessages } from "./timelineAPI";
 
+interface Thread {
+    first: number;
+    last: number;
+    count: number;
+}
+
+type ThreadLookup = { [threadId: string]: Thread };
+type ThreadIndentationLookup = { [threadId: string]: number };
+
+function determineHighlightedThreads(messages: IMessage[]): ThreadLookup {
+    const threads: { [threadId: string]: Thread } = {};
+    for (const message of messages) {
+        if (!threads[message.threadId]) {
+            threads[message.threadId] = {
+                first: message.timeRank,
+                last: message.timeRank,
+                count: 1,
+            };
+        } else {
+            threads[message.threadId].count += 1;
+            threads[message.threadId].last = message.timeRank;
+        }
+    }
+    for (const threadId in threads) {
+        if (threads[threadId].count < 2) {
+            delete threads[threadId];
+        }
+    }
+    return threads;
+}
+
+function determineIndentation(messages: IMessage[]): ThreadIndentationLookup {
+    const highlightedThreads = determineHighlightedThreads(messages);
+    const indentations: ThreadIndentationLookup = {};
+    const currentlyAllocated = new Set<number>();
+
+    for (const { threadId, timeRank } of messages) {
+        const messageThread = highlightedThreads[threadId];
+        if (messageThread) {
+            if (messageThread.first === timeRank) {
+                // Is first element of thread
+                for (let i = 1; i <= messages.length; ++i) {
+                    if (!currentlyAllocated.has(i)) {
+                        indentations[threadId] = i;
+                        currentlyAllocated.add(i);
+                        break;
+                    }
+                }
+            } else if (messageThread.last === timeRank) {
+                // Is last element of thread
+                currentlyAllocated.delete(indentations[threadId]);
+            }
+        }
+    }
+    return indentations;
+}
+
 export interface TimelineState {
     messages: { [timeRank: number]: IMessage };
-    order: number[];
+    threadIndentation: ThreadIndentationLookup;
 }
 
 const initialState: TimelineState = {
     messages: {},
-    order: [],
+    threadIndentation: {},
 };
 
 export const timelineFetchAsync = createAsyncThunk(
@@ -26,11 +83,13 @@ const timelineSlice = createSlice({
     reducers: {},
     extraReducers: (builder) => {
         builder.addCase(timelineFetchAsync.fulfilled, (_state, action) => {
+            const threadIndentation = determineIndentation(action.payload);
+            const messages = Object.fromEntries(
+                action.payload.map((message) => [message.timeRank, message])
+            );
             return {
-                messages: Object.fromEntries(
-                    action.payload.map((message) => [message.timeRank, message])
-                ),
-                order: [],
+                messages,
+                threadIndentation,
             };
         });
     },
